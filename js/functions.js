b6314b6868704c0e31ba05b1ced8b101f6c2c796
Yartseva Ksenia <kseniyasid@gmail.com>
2023-06-14 14:43:30 +0500

    Пишет функцию для проверки на палиндром

--- js/functions.js
+++ js/functions.js
@@ -2,5 +2,18 @@
 function checkStringLength(string,length) {
   return string.length <= length;
 }
+
 // Функция для проверки, является ли строка палиндромом.
+function checkPalindrome(string) {
+  const stringWithoutSpaces = string.replaceAll(' ','');
+  const newString = stringWithoutSpaces.toLowerCase();
+  let stringMirror = '';
+
+  for (let i = newString.length - 1 ; i >= 0 ; i--) {
+    const letter = newString[i];
+    stringMirror += letter;
+  }
+
+  return newString === stringMirror;
+}
 //Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
