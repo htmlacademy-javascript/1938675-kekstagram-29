a513cbcbb4b4347973f89edcf01ee9caaddf972e
Yartseva Ksenia <kseniyasid@gmail.com>
2023-06-15 13:07:27 +0500

    Делает рефакторинг функций

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,11 @@
 // Функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее
-function checkStringLength(string,length) {
-  return string.length <= length;
-}
+const checkStringLength = (string,length) => string.length <= length;
+
+checkStringLength('Привет', 60);
 
 // Функция для проверки, является ли строка палиндромом.
-function checkPalindrome(string) {
-  const stringWithoutSpaces = string.replaceAll(' ','');
-  const newString = stringWithoutSpaces.toLowerCase();
+const checkPalindrome = (string) => {
+  const newString = string.replaceAll(' ','').toLowerCase();
   let stringMirror = '';
 
   for (let i = newString.length - 1 ; i >= 0 ; i--) {
@@ -15,5 +14,23 @@ function checkPalindrome(string) {
   }
 
   return newString === stringMirror;
-}
-//Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
+};
+
+checkPalindrome('Мама мыла Милу мылом');
+
+//Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN. Предусмотрите случай, когда вместо строки приходит число.
+/*function getNumber(string) {
+  let number = '';
+
+  for(let i = 0 ; i < string.length ; i++) {
+    const NumberIsNan = Number.isNaN(parseInt(string[i],10));
+
+    const NumberIsNan===true ? number += string[i] : number = '';
+  }
+}*/
+
+console.log('Ожидаю 2023 — получаю ' + getNumber('2023 год'));
+//console.log('Ожидаю 2022 — получаю ' + getNumber('ECMAScript 2022'));
+//console.log('Ожидаю 105 — получаю ' + getNumber('1 кефир, 0.5 батона'));
+//console.log('Ожидаю 7 — получаю ' + getNumber('агент 007'));
+//console.log('Ожидаю NaN — получаю ' + getNumber('а я томат'));
