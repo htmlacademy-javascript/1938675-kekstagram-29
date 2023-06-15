61318201c74b3e2556eef22952c426ea5755b8f2
Yartseva Ksenia <kseniyasid@gmail.com>
2023-06-15 15:00:06 +0500

    Пишет функцию, которая извлекает число из строки

--- js/functions.js
+++ js/functions.js
@@ -19,18 +19,15 @@ const checkPalindrome = (string) => {
 checkPalindrome('Мама мыла Милу мылом');
 
 //Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN. Предусмотрите случай, когда вместо строки приходит число.
-/*function getNumber(string) {
+const getNumberFromString = (string) => {
   let number = '';
 
   for(let i = 0 ; i < string.length ; i++) {
     const NumberIsNan = Number.isNaN(parseInt(string[i],10));
-
-    const NumberIsNan===true ? number += string[i] : number = '';
+    number = NumberIsNan === true ? number : number + string[i];
   }
-}*/
 
-console.log('Ожидаю 2023 — получаю ' + getNumber('2023 год'));
-//console.log('Ожидаю 2022 — получаю ' + getNumber('ECMAScript 2022'));
-//console.log('Ожидаю 105 — получаю ' + getNumber('1 кефир, 0.5 батона'));
-//console.log('Ожидаю 7 — получаю ' + getNumber('агент 007'));
-//console.log('Ожидаю NaN — получаю ' + getNumber('а я томат'));
+  return Number(parseInt(number,10));
+};
+
+getNumberFromString('2023 год');
