b31c757ee484833adec0113dc458802b91dca3c5
Yartseva Ksenia <kseniyasid@gmail.com>
2023-07-10 14:10:39 +0500

    Добавляет комментарии в большое фото

--- js/full-photo.js
+++ js/full-photo.js
@@ -8,6 +8,31 @@ const pictureImgElement = document.querySelector('.big-picture__img img');
 const likesCountElement = document.querySelector('.likes-count');
 const commentsCountElement = document.querySelector('.comments-count');
 const pictureDescriptionElement = document.querySelector('.social__caption');
+const commentsList = pictureWrapper.querySelector('.social__comments');
+const commentTemplate = commentsList.querySelector('.social__comment');
+
+const createComment = ({avatar, message, name}) => {
+  const commentElement = commentTemplate.cloneNode(true);
+  const commentAvatar = commentElement.querySelector('.social__picture');
+  const commentText = commentElement.querySelector('.social__text');
+
+  commentAvatar.src = avatar;
+  commentAvatar.alt = name;
+  commentText.textContent = message;
+
+  return commentElement;
+};
+
+const renderComments = (comments) => {
+  commentsList.innerHTML = '';
+  const fragment = document.createDocumentFragment();
+  comments.forEach((element) => {
+    const comment = createComment(element);
+    fragment.append(comment);
+  });
+
+  commentsList.append(fragment);
+};
 
 const openBigPicture = ({url, description, likes, comments}) => {
   pictureWrapper.classList.remove('hidden');
@@ -19,6 +44,8 @@ const openBigPicture = ({url, description, likes, comments}) => {
   commentsCountElement.textContent = comments;
   likesCountElement.textContent = likes;
   pictureDescriptionElement.alt = description;
+
+  renderComments(comments);
 
   document.addEventListener('keydown', (evt) => {
     if (isEscapeKey(evt)) {
