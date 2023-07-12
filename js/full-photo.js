e24dc7990a5ee3d7c0cf6ee7880e0538580803de
Yartseva Ksenia <kseniyasid@gmail.com>
2023-07-12 14:24:19 +0500

    Переносит все по комментариям в отдельный модуль

--- js/full-photo.js
+++ js/full-photo.js
@@ -1,40 +1,11 @@
-import {isEscapeKey} from './util.js';
-
-const COMMENTS_PACK_AMOUNT = 5;
+import { isEscapeKey } from './util.js';
+import { renderComments, clearComments } from './comments.js';
 
 const pictureWrapper = document.querySelector('.big-picture');
 const closeButton = document.querySelector('.big-picture__cancel');
-const socialCommentCount = document.querySelector('.social__comment-count');
-const commentsLoader = document.querySelector('.comments-loader');
 const pictureImgElement = document.querySelector('.big-picture__img img');
 const likesCountElement = document.querySelector('.likes-count');
-const commentsCountElement = document.querySelector('.comments-count');
 const pictureDescriptionElement = document.querySelector('.social__caption');
-const commentsList = pictureWrapper.querySelector('.social__comments');
-const commentTemplate = commentsList.querySelector('.social__comment');
-
-const createComment = ({avatar, message, name}) => {
-  const commentElement = commentTemplate.cloneNode(true);
-  const commentAvatar = commentElement.querySelector('.social__picture');
-  const commentText = commentElement.querySelector('.social__text');
-
-  commentAvatar.src = avatar;
-  commentAvatar.alt = name;
-  commentText.textContent = message;
-
-  return commentElement;
-};
-
-const renderComments = (comments) => {
-  commentsList.innerHTML = '';
-  const fragment = document.createDocumentFragment();
-  comments.forEach((element) => {
-    const comment = createComment(element);
-    fragment.append(comment);
-  });
-
-  commentsList.append(fragment);
-};
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -44,15 +15,16 @@ const onDocumentKeydown = (evt) => {
 };
 
 const openBigPicture = ({url, description, likes, comments}) => {
+  clearComments();
+
   pictureWrapper.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
   pictureImgElement.src = url;
-  commentsCountElement.textContent = comments.length;
   likesCountElement.textContent = likes;
   pictureDescriptionElement.alt = description;
 
-  renderComments(comments.slice(0, COMMENTS_PACK_AMOUNT));
+  renderComments(comments);
 
   document.addEventListener('keydown', onDocumentKeydown);
 };
@@ -63,6 +35,7 @@ function closeBigPicture() {
   pictureWrapper.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
+  clearComments();
 }
 
 export {openBigPicture};
