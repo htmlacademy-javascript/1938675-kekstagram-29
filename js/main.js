3417574259c3c4a98712b59fb60ee5b5ed4126eb
Yartseva Ksenia <kseniyasid@gmail.com>
2023-07-25 19:04:12 +0500

    Рендерит фото с сервера вместо моков

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 import './upload-form/form-image-upload.js';
-import './thumbnails.js';
 import './upload-form/scale.js';
 import './upload-form/effects.js';
 import './upload-form/validation.js';
+import { renderPack } from './util.js';
+import { pictureList, createPictureMarkUp } from './thumbnails.js';
+
+fetch('https://29.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((photos) => {
+    renderPack(photos, pictureList, createPictureMarkUp);
+  });
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,36 +1,23 @@
-import {getMockedPhotos} from './data.js';
-import {openBigPicture} from './full-photo.js';
+import { openBigPicture } from './full-photo.js';
 
 const pictureList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const pictures = getMockedPhotos();
-
-const createPictureMarkUp = ({description, comments, likes, url}) => {
+const createPictureMarkUp = (picture) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   const imageElement = pictureElement.querySelector('.picture__img');
 
-  imageElement.src = url;
-  imageElement.alt = description;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
+  imageElement.src = picture.url;
+  imageElement.alt = picture.description;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+
+  pictureElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openBigPicture(picture);
+  });
 
   return pictureElement;
 };
 
-const pictureListFragment = document.createDocumentFragment();
-
-const renderPicture = ((photo) => {
-  const pictureElement = createPictureMarkUp(photo);
-  pictureElement.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    openBigPicture(photo);
-  });
-
-  pictureListFragment.appendChild(pictureElement);
-});
-
-pictures.forEach(renderPicture);
-pictureList.appendChild(pictureListFragment);
-
-export {renderPicture};
+export { pictureList, createPictureMarkUp };
