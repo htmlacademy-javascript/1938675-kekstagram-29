d169d8299ed2c6ce214aabb696252bc5dad9f033
Yartseva Ksenia <kseniyasid@gmail.com>
2023-07-31 23:51:24 +0500

    Добавляет показ болка с фильтрами

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ import { renderPack } from './util.js';
 import { pictureList, createPictureMarkUp } from './thumbnails.js';
 import { setUserFormSubmit } from './upload-form/validation.js';
 import { closeForm } from './upload-form/form-image-upload.js';
+import { showFilters } from './upload-form/filters.js';
 
 fetch('https://29.javascript.pages.academy/kekstagram/data')
   .then((response) => response.json())
   .then((photos) => {
     renderPack(photos, pictureList, createPictureMarkUp);
-  });
+  })
+  .then(showFilters());
 
 setUserFormSubmit(closeForm);
