1e89336a55ddfd64838afc5c3ad63b3589513b06
Yartseva Ksenia <kseniyasid@gmail.com>
2023-07-31 21:32:46 +0500

    Закрывает сообщение по клику вне модалки

--- js/upload-form/success-error-messages.js
+++ js/upload-form/success-error-messages.js
@@ -3,10 +3,12 @@ import { isEscapeKey } from '../util.js';
 const successTemplate = document.querySelector('#success').content.querySelector('.success');
 const successWindow = successTemplate.cloneNode(true);
 const successBtn = successWindow.querySelector('.success__button');
+const successInner = successWindow.querySelector('.success__inner');
 
 const errorTemplate = document.querySelector('#error').content.querySelector('.error');
 const errorWindow = errorTemplate.cloneNode(true);
 const errorBtn = errorWindow.querySelector('.error__button');
+const errorInner = errorWindow.querySelector('.error__inner');
 
 const removeErrorMessageWindow = () => {
   errorWindow.remove();
@@ -19,10 +21,17 @@ const onDocumentKeydownError = (evt) => {
   }
 };
 
+const onClickOverlayError = (evt) => {
+  if (evt.target !== errorInner) {
+    removeErrorMessageWindow();
+  }
+};
+
 const showErrorMessage = () => {
   document.body.append(errorWindow);
   errorBtn.addEventListener('click', removeErrorMessageWindow);
   document.addEventListener('keydown', onDocumentKeydownError);
+  document.addEventListener('click', onClickOverlayError);
 };
 
 const removeSuccessMessageWindow = () => {
@@ -36,10 +45,17 @@ const onDocumentKeydownSuccess = (evt) => {
   }
 };
 
+const onClickOverlaySuccess = (evt) => {
+  if (evt.target !== successInner) {
+    removeSuccessMessageWindow();
+  }
+};
+
 const showSuccessMessage = () => {
   document.body.append(successWindow);
   successBtn.addEventListener('click', removeSuccessMessageWindow);
   document.addEventListener('keydown', onDocumentKeydownSuccess);
+  document.addEventListener('click', onClickOverlaySuccess);
 };
 
 export { showErrorMessage, showSuccessMessage };
