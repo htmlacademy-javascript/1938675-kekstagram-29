3b91f06066abad9d7cb1bf78c5401d8d54739fc7
Yartseva Ksenia <kseniyasid@gmail.com>
2023-08-01 17:14:01 +0500

    Доделывает логику фильтров

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ import './upload-form/effects.js';
 import { renderPhotos } from './thumbnails.js';
 import { setUserFormSubmit } from './upload-form/validation.js';
 import { closeForm } from './upload-form/form-image-upload.js';
-import { setFilters } from './upload-form/filters.js';
+import { showFilters, setFilters } from './upload-form/filters.js';
 
 fetch('https://29.javascript.pages.academy/kekstagram/data')
   .then((response) => response.json())
   .then((photos) => {
     renderPhotos(photos);
+    showFilters();
     setFilters(photos);
   });
 
--- js/upload-form/filters.js
+++ js/upload-form/filters.js
@@ -1,13 +1,14 @@
 import { renderPhotos } from '../thumbnails.js';
-import { sortRandom } from '../util.js';
+import { sortRandom, debounce } from '../util.js';
 
-//const TIMEOUT = 500;
+const TIMEOUT = 500;
 const PICTURES_COUNT = 10;
 
 const filters = document.querySelector('.img-filters');
-const filterDefault = document.getElementById('filter-default');
-const filterRandom = document.getElementById('filter-random');
-const filterDiscussed = document.getElementById('filter-discussed');
+const filterForm = document.querySelector('.img-filters__form');
+const filterDefaultBtn = document.getElementById('filter-default');
+const filterRandomBtn = document.getElementById('filter-random');
+const filterDiscussedBtn = document.getElementById('filter-discussed');
 
 const removeThumbnails = (thumbnails) => thumbnails.forEach((thumbnail) => thumbnail.remove());
 
@@ -15,30 +16,30 @@ const compareFunction = (a, b) => b.comments.length - a.comments.length;
 
 const showFilters = () => filters.classList.remove('img-filters--inactive');
 
-const sortItemsByDiscussion = (items) => items.slice().sort(compareFunction);
-
-const sortItemsRandom = (items) => items.slice().sort(sortRandom).slice(0, PICTURES_COUNT);
-
-const setFilters = (photos) => {
-  showFilters();
-
-  filterDiscussed.addEventListener('click', () => {
-    const thumbnails = document.querySelectorAll('.picture');
-    removeThumbnails(thumbnails);
-    renderPhotos(sortItemsByDiscussion(photos));
-  });
-
-  filterRandom.addEventListener('click', () => {
-    const thumbnails = document.querySelectorAll('.picture');
-    removeThumbnails(thumbnails);
-    renderPhotos(sortItemsRandom(photos));
-  });
-
-  filterDefault.addEventListener('click', () => {
-    const thumbnails = document.querySelectorAll('.picture');
-    removeThumbnails(thumbnails);
-    renderPhotos(photos);
-  });
+const filterPhotos = (photos, filterBtn) => {
+  if(filterBtn === filterDefaultBtn) {
+    return photos;
+  } else if(filterBtn === filterRandomBtn) {
+    return photos.slice().sort(sortRandom).slice(0, PICTURES_COUNT);
+  } else if(filterBtn === filterDiscussedBtn) {
+    return photos.slice().sort(compareFunction);
+  }
 };
 
-export { setFilters };
+const onFilterButtonClick = (event, photos) => {
+  const thumbnails = document.querySelectorAll('.picture');
+  const filterBtn = event.target;
+  const activeButton = document.querySelector('.img-filters__button--active');
+  activeButton.classList.remove('img-filters__button--active');
+  filterBtn.classList.add('img-filters__button--active');
+  removeThumbnails(thumbnails);
+  renderPhotos(filterPhotos(photos, filterBtn));
+};
+
+const setFilters = (photos) => {
+  filterForm.addEventListener('click', debounce((event) => {
+    onFilterButtonClick(event, photos);
+  }, TIMEOUT));
+};
+
+export { showFilters, setFilters };
