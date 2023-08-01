360c6a90a6105f6316d630c095ac832a76077340
Yartseva Ksenia <kseniyasid@gmail.com>
2023-08-01 19:47:11 +0500

    Доделывает модуль с апи

--- js/api.js
+++ js/api.js
@@ -4,16 +4,30 @@ const Route = {
   SEND_DATA: '/',
 };
 
-const getData = () => fetch(
-  `${BASE_URL}${Route.GET_DATA}`)
-  .then((response) => response.json());
+const ErrorText = {
+  GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+  SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
+};
 
-const sendData = (body) => fetch(
-  `${BASE_URL}${Route.SEND_DATA}`,
-  {
-    method: 'POST',
-    body,
-  },
-);
+const Method = {
+  GET: 'GET',
+  POST: 'POST',
+};
+
+const load = (route, errorText = null, method = Method.GET, body = null) =>
+  fetch(`${BASE_URL}${route}`, {method, body})
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Произошла ошибка ${response.status}: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .catch((err) => {
+      throw new Error(errorText ?? err.message);
+    });
+
+const getData = () => load(Route.GET_DATA);
+
+const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
 
 export {getData, sendData};
