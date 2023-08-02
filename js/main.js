31c23376242217584e19bf984bc85998ad8dd0b0
Yartseva Ksenia <kseniyasid@gmail.com>
2023-08-02 14:42:06 +0500

    Удаляет лишний импорт

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import './upload-form/scale.js';
 import './upload-form/effects.js';
-import { renderPhotos } from './thumbnails.js';
 import { setUserFormSubmit } from './upload-form/validation.js';
 import { closeForm } from './upload-form/form-image-upload.js';
 import { setFilters } from './upload-form/filters.js';
